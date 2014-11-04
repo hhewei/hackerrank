524a9dd1353ffe9379d4505bf15d894266352cd4
Wei He <hewei.com@gmail.com>
2014-11-05 00:27:43 +0800

    use Data.Bits to represent board

--- recursion/super-queens-on-a-chessboard/Main.hs
+++ recursion/super-queens-on-a-chessboard/Main.hs
@@ -1,32 +1,33 @@
 module Main where
 
-import Data.ByteString.Char8
+import Data.Bits
 
 newtype Board = Board {
-  squares :: ByteString
+  bits :: Integer
   }
 
 sqoc :: Int -> Integer
 sqoc n = sqoc' initial 0 where
-  initial = Board$ pack [' ' | _ <- [1..n*n]]
+  initial = Board 0
   sqoc' b i
     | i == n = 1
     | otherwise = sum [sqoc' (place b (i,j) n) (i+1) | j <- [0..n-1], can b (i,j) n]
 
 can :: Board -> (Int, Int) -> Int -> Bool
-can (Board s) (i,j) n = s`index`(i*n+j) == ' ' && [] == ['-'| x <- [0..i], y <- [0..j], s`index`(x*n+y) == 'q', (x,y) `conflict` (i,j)]
+can (Board s) (i,j) n = not$ s`testBit`(i*n+j)
 
 place :: Board -> (Int, Int) -> Int -> Board
 place (Board s) (i,j) n =
-  Board$ pack [ if (x,y) == (i,j) then 'q'
-          else if (x,y) `conflict` (i,j) then '-' else s`index`(x*n+y)
-        | x <- [0..n-1], y <- [0..n-1]]
+  Board$ foldl setBit s [x*n+y | x <- [i..n-1], y <- [0..n-1], not$ s`testBit`(x*n+y), (x,y) `conflict` (i,j)]
 
 conflict :: (Int, Int) -> (Int, Int) -> Bool
-conflict (x,y) (i,j) | x == i || y == j = True
-                     | abs(x-i) == abs(y-j) = True
-                     | abs(x-i) + abs(y-j) <= 3 = True
-                     | otherwise = False
+conflict (x,y) (i,j)
+  | x == i || y == j = True
+  | dxi == dyj = True
+  | dxi + dyj <= 3 = True
+  | otherwise = False
+  where dxi = abs(x-i)
+        dyj = abs(y-j)
 
 main :: IO ()
 main = do
