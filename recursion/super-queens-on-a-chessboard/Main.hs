b95177295ace081efc3608ba732da3418f39a306
Wei He <hewei.com@gmail.com>
2014-11-11 00:21:11 +0800

    Bit patterns and bitwise-or.

--- recursion/super-queens-on-a-chessboard/Main.hs
+++ recursion/super-queens-on-a-chessboard/Main.hs
@@ -1,40 +1,30 @@
 module Main where
 
 import Data.Bits
+import Data.List (nub)
+import Data.IntMap (IntMap, fromList, (!))
 
 newtype Board = Board {
   bits :: Integer
   }
 
 sqoc :: Int -> Integer
-sqoc n = sqoc' initial 0 where
-  initial = Board 0
+sqoc n = sqoc' (Board 0) 0 where
   sqoc' b i
     | i == n = 1
-    | otherwise = sum [sqoc' (place b (i,j) n) (i+1) | j <- [0..n-1], can b (i,j) n]
+    | otherwise = sum [sqoc' (place b (i,j)) (i+1) | j <- [0..n-1], can b (i,j)]
+  
+  can :: Board -> (Int, Int) -> Bool
+  can (Board s) (i,j) = not$ s`testBit`(i*n+j)
 
-can :: Board -> (Int, Int) -> Int -> Bool
-can (Board s) (i,j) n = not$ s`testBit`(i*n+j)
+  place :: Board -> (Int, Int) -> Board
+  place (Board s) (i,j) = Board$ s .|. (patterns!(i*n+j))
 
-place :: Board -> (Int, Int) -> Int -> Board
-place (Board s) (i,j) n =
-  --Board$ foldl setBit s [x*n+y | x <- [i..n-1], y <- [0..n-1], not$ s`testBit`(x*n+y), (x,y) `conflict` (i,j)]
-  Board$ f s (i*n+j)
-  where
-    f p l | l == n*n = p
-          | (not$ s`testBit`l) && (x,y) `conflict` (i,j) = f (p `setBit` l) (l+1)
-          | otherwise = f p (l+1)
-      where (x, y) = (l`div`n, l`mod`n)
-
-
-conflict :: (Int, Int) -> (Int, Int) -> Bool
-conflict (x,y) (i,j)
-  | x == i || y == j = True
-  | dxi == dyj = True
-  | dxi + dyj <= 3 = True
-  | otherwise = False
-  where dxi = abs(x-i)
-        dyj = abs(y-j)
+  patterns :: IntMap Integer
+  patterns = fromList [(i*n+j, pattern i j) | i <- [0..n-1], j <- [0..n-1]] where
+    pattern i j = foldl setBit 0 [i'*n+j' | (di,dj) <- offsets, let i' = i+di, let j' = j+dj, on i', on j']
+    on x = 0 <= x && x < n
+    offsets = nub$ [(i,j) | i <- [-2..2], j <- [-2..2]] ++ concat [[(0,i),(i,0),(i,i),(i,-i)] | i <- [-(n-1)..(n-1)]]
 
 main :: IO ()
 main = do
